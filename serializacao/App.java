ea794340741a4db6ca540e95be3f8ec8c2be7cb0
Henrique Mendonça <91921212+HenriqueGMen@users.noreply.github.com>
2022-08-05 16:06:34 -0300

    Create App.java

---
+++ serializacao/App.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        serializarProduto();
+        desserializarProduto();
+    }
+
+    private static void serializarProduto() {
+        Produto produto1 = new Produto("Camisa", 100);
+        try {
+            FileOutputStream arquivoProduto = new FileOutputStream("carrinho.bin");
+            ObjectOutputStream saidaProduto = new ObjectOutputStream(arquivoProduto);
+            saidaProduto.writeObject(produto1);
+            saidaProduto.close();
+        } catch (IOException e) {
+            System.out.println("Erro");
+        }
+    }
+
+    private static void desserializarProduto() {
+        try {
+            FileInputStream arquivoProdutoEntrada = new FileInputStream("carrinho.bin");
+            ObjectInputStream entradaProduto = new ObjectInputStream(arquivoProdutoEntrada);
+            Produto  produto1 = (Produto) entradaProduto.readObject();
+            System.out.printf("Nome: %s\nValor: %.2f", produto1.getNome(), produto1.getValor());
+            entradaProduto.close();
+        } catch (IOException e) {
+            System.out.println("Erro");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
