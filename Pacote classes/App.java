23d619e8932bcc90a8cbe8850818e592d474ae03
Henrique Mendonça <91921212+HenriqueGMen@users.noreply.github.com>
2022-08-11 10:37:50 -0300

    Update App.java

--- Pacote classes/App.java
+++ Pacote classes/App.java
@@ -1,1 +1,22 @@
+import Transportadora.*;
+import Lojas.*;
 
+public class App {
+  public static void main(String[] args) {
+    Produto primeiroProduto = new Produto("Camisa", 119.99);
+    primeiroProduto.setProduto("Calça");
+    primeiroProduto.setPreco(200);    
+    System.out.println(primeiroProduto.getProduto());
+    System.out.println(primeiroProduto.getPreco());
+
+
+    Filial filialLoja = new Filial("Recife");
+    filialLoja.setLocalidade("Caruaru");
+    System.out.println(filialLoja.getLocalidade());
+
+
+    Transportadora tranporta = new Transportadora("ABC Transports", "Caminhão");
+    tranporta.setVeiculo("Moto");
+    System.out.println(tranporta.getVeiculo());  
+  }
+}
